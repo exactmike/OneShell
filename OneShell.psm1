477b9b35548160c7f77c62647cdcdcafc7330a08
exactmike <mike@exactsolutions.biz>
2016-04-21 14:03:56 -0400

    Fix a bug in Test-ExchangeProxyAddress (yikes).  Add Get-AllADRecipientObjects function

--- OneShell.psm1
+++ OneShell.psm1
@@ -893,8 +893,8 @@ else {
     RefreshData
 }
 #Fix the ProxyAddress if needed
-if ($ProxyAddress -notlike "{$proxyaddresstype}:*") {
-    $ProxyAddress = "${proxyaddresstype}:$ProxyAddress"
+if ($ProxyAddress -notlike "$($proxyaddresstype):*") {
+    $ProxyAddress = "$($proxyaddresstype):$ProxyAddress"
 }
 #Test the ProxyAddress
 if ($global:TestExchangeProxyAddress.ContainsKey($ProxyAddress)) {
@@ -930,7 +930,7 @@ param(
 
 #Fix the ProxyAddress if needed
 if ($ProxyAddress -notlike "{$proxyaddresstype}:*") {
-    $ProxyAddress = "${proxyaddresstype}:$ProxyAddress"
+    $ProxyAddress = "${$proxyaddresstype}:$ProxyAddress"
 }
 #Test the Proxy Address
 if ($Global:TestExchangeProxyAddress.ContainsKey($ProxyAddress))
@@ -3522,6 +3522,32 @@ function Get-XADUserPasswordExpirationDate() {
 
     }
 
+}
+function Get-AllADRecipientObjects {
+[cmdletbinding()]
+param
+(
+[int]$ResultSetSize = 10000
+,
+[switch]$Passthrough
+,
+[switch]$ExportData
+,
+[parameter(Mandatory=$true)]
+$ADInstance
+)
+    cd "$($ADInstance):\"
+    $AllGroups = Get-ADGroup -ResultSetSize $ResultSetSize -Properties @($AllADAttributesToRetrieve + 'Members') -Filter * | Select-Object -Property * -ExcludeProperty Property*,Item
+    $AllMailEnabledGroups = $AllGroups | Where-Object -FilterScript {$_.legacyExchangeDN -ne $NULL -or $_.mailNickname -ne $NULL -or $_.proxyAddresses -ne $NULL}
+    $AllContacts = Get-ADObject -Filter {objectclass -eq 'contact'} -Properties $AllADContactAttributesToRetrieve -ResultSetSize $ResultSetSize | Select-Object -Property * -ExcludeProperty Property*,Item
+    $AllMailEnabledContacts = $AllContacts | Where-Object -FilterScript {$_.legacyExchangeDN -ne $NULL -or $_.mailNickname -ne $NULL -or $_.proxyAddresses -ne $NULL}
+    $AllUsers = Get-ADUser -ResultSetSize $ResultSetSize -Filter * -Properties $AllADAttributesToRetrieve | Select-Object -Property * -ExcludeProperty Property*,Item
+    $AllMailEnabledUsers = $AllUsers  | Where-Object -FilterScript {$_.legacyExchangeDN -ne $NULL -or $_.mailNickname -ne $NULL -or $_.proxyAddresses -ne $NULL}
+    $AllPublicFolders = Get-ADObject -Filter {objectclass -eq 'publicFolder'} -ResultSetSize $ResultSetSize -Properties $AllADAttributesToRetrieve | Select-Object -Property * -ExcludeProperty Property*,Item
+    $AllMailEnabledPublicFolders = $AllPublicFolders  | Where-Object -FilterScript {$_.legacyExchangeDN -ne $NULL -or $_.mailNickname -ne $NULL -or $_.proxyAddresses -ne $NULL}
+    $AllMailEnabledADObjects = $AllMailEnabledGroups + $AllMailEnabledContacts + $AllMailEnabledUsers + $AllMailEnabledPublicFolders
+    if ($Passthrough) {$AllMailEnabledADObjects}
+    if ($ExportData) {Export-Data -DataToExport $AllMailEnabledADObjects -DataToExportTitle 'AllADRecipientObjects' -Depth 3 -DataType xml}
 }
 ##########################################################################################################
 #Profile and Environment Initialization Functions
