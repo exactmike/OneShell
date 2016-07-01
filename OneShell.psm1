a92f2fdfc8f4695801e1b1540f81469d71e96500
Mike Campbell <mike@exactsolutions.biz>
2016-06-30 22:16:19 -0400

    add function Get-CallerPreference and modify AD Attribute retrieval variables

--- OneShell.psm1
+++ OneShell.psm1
@@ -223,6 +223,154 @@ A custom non-terminating ErrorRecord is stored in variable 'errorRecord' and the
     ModifiedBy: Mike Campbell
 #>
 }
+#requires -Version 2.0
+function Get-CallerPreference
+{
+    <#
+    .Synopsis
+       Fetches "Preference" variable values from the caller's scope.
+    .DESCRIPTION
+       Script module functions do not automatically inherit their caller's variables, but they can be
+       obtained through the $PSCmdlet variable in Advanced Functions.  This function is a helper function
+       for any script module Advanced Function; by passing in the values of $ExecutionContext.SessionState
+       and $PSCmdlet, Get-CallerPreference will set the caller's preference variables locally.
+    .PARAMETER Cmdlet
+       The $PSCmdlet object from a script module Advanced Function.
+    .PARAMETER SessionState
+       The $ExecutionContext.SessionState object from a script module Advanced Function.  This is how the
+       Get-CallerPreference function sets variables in its callers' scope, even if that caller is in a different
+       script module.
+    .PARAMETER Name
+       Optional array of parameter names to retrieve from the caller's scope.  Default is to retrieve all
+       Preference variables as defined in the about_Preference_Variables help file (as of PowerShell 4.0)
+       This parameter may also specify names of variables that are not in the about_Preference_Variables
+       help file, and the function will retrieve and set those as well.
+    .EXAMPLE
+       Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+
+       Imports the default PowerShell preference variables from the caller into the local scope.
+    .EXAMPLE
+       Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState -Name 'ErrorActionPreference','SomeOtherVariable'
+
+       Imports only the ErrorActionPreference and SomeOtherVariable variables into the local scope.
+    .EXAMPLE
+       'ErrorActionPreference','SomeOtherVariable' | Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+
+       Same as Example 2, but sends variable names to the Name parameter via pipeline input.
+    .INPUTS
+       String
+    .OUTPUTS
+       None.  This function does not produce pipeline output.
+    .LINK
+       about_Preference_Variables
+    #>
+    #https://gallery.technet.microsoft.com/scriptcenter/Inherit-Preference-82343b9d
+    [CmdletBinding(DefaultParameterSetName = 'AllVariables')]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ $_.GetType().FullName -eq 'System.Management.Automation.PSScriptCmdlet' })]
+        $Cmdlet,
+
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.SessionState]
+        $SessionState,
+
+        [Parameter(ParameterSetName = 'Filtered', ValueFromPipeline = $true)]
+        [string[]]
+        $Name
+    )
+    begin
+    {
+        $filterHash = @{}
+    }
+    process
+    {
+        if ($null -ne $Name)
+        {
+            foreach ($string in $Name)
+            {
+                $filterHash[$string] = $true
+            }
+        }
+    }
+    end
+    {
+        # List of preference variables taken from the about_Preference_Variables help file in PowerShell version 4.0
+        $vars = @{
+            'ErrorView' = $null
+            'FormatEnumerationLimit' = $null
+            'LogCommandHealthEvent' = $null
+            'LogCommandLifecycleEvent' = $null
+            'LogEngineHealthEvent' = $null
+            'LogEngineLifecycleEvent' = $null
+            'LogProviderHealthEvent' = $null
+            'LogProviderLifecycleEvent' = $null
+            'MaximumAliasCount' = $null
+            'MaximumDriveCount' = $null
+            'MaximumErrorCount' = $null
+            'MaximumFunctionCount' = $null
+            'MaximumHistoryCount' = $null
+            'MaximumVariableCount' = $null
+            'OFS' = $null
+            'OutputEncoding' = $null
+            'ProgressPreference' = $null
+            'PSDefaultParameterValues' = $null
+            'PSEmailServer' = $null
+            'PSModuleAutoLoadingPreference' = $null
+            'PSSessionApplicationName' = $null
+            'PSSessionConfigurationName' = $null
+            'PSSessionOption' = $null
+            'ErrorActionPreference' = 'ErrorAction'
+            'DebugPreference' = 'Debug'
+            'ConfirmPreference' = 'Confirm'
+            'WhatIfPreference' = 'WhatIf'
+            'VerbosePreference' = 'Verbose'
+            'WarningPreference' = 'WarningAction'
+        }
+        foreach ($entry in $vars.GetEnumerator())
+        {
+            if (([string]::IsNullOrEmpty($entry.Value) -or -not $Cmdlet.MyInvocation.BoundParameters.ContainsKey($entry.Value)) -and
+                ($PSCmdlet.ParameterSetName -eq 'AllVariables' -or $filterHash.ContainsKey($entry.Name)))
+            {
+                $variable = $Cmdlet.SessionState.PSVariable.Get($entry.Key)
+                
+                if ($null -ne $variable)
+                {
+                    if ($SessionState -eq $ExecutionContext.SessionState)
+                    {
+                        Set-Variable -Scope 1 -Name $variable.Name -Value $variable.Value -Force -Confirm:$false -WhatIf:$false
+                    }
+                    else
+                    {
+                        $SessionState.PSVariable.Set($variable.Name, $variable.Value)
+                    }
+                }
+            }
+        }
+        if ($PSCmdlet.ParameterSetName -eq 'Filtered')
+        {
+            foreach ($varName in $filterHash.Keys)
+            {
+                if (-not $vars.ContainsKey($varName))
+                {
+                    $variable = $Cmdlet.SessionState.PSVariable.Get($varName)
+                
+                    if ($null -ne $variable)
+                    {
+                        if ($SessionState -eq $ExecutionContext.SessionState)
+                        {
+                            Set-Variable -Scope 1 -Name $variable.Name -Value $variable.Value -Force -Confirm:$false -WhatIf:$false
+                        }
+                        else
+                        {
+                            $SessionState.PSVariable.Set($variable.Name, $variable.Value)
+                        }
+                    }
+                }
+            }
+        }
+    } # end
+} # function Get-CallerPreference
 #Useful Functions
 function Get-CustomRange
 {
@@ -5359,7 +5507,7 @@ function Set-OneShellVariables
     [string]$Script:K1_SkuPartNumber = 'DESKLESSPACK' 
     $Script:LogPreference = $True
     #AdvancedOneShell needs updated for the following:
-    $Script:ScalarADAttributesToRetrieve = @(
+    $Script:ScalarADAttributes = @(
         'altRecipient'
         'forwardingAddress'
         'msExchGenericForwardingAddress'
@@ -5417,7 +5565,7 @@ function Set-OneShellVariables
         'country'
         'physicalDeliveryOfficeName'
     )#Scalar Attributes to Retrieve
-    $Script:MultiValuedADAttributesToRetrieve = @(
+    $Script:MultiValuedADAttributes = @(
         'proxyAddresses'
         'msexchextensioncustomattribute1'
         'msexchextensioncustomattribute2'
@@ -5427,8 +5575,10 @@ function Set-OneShellVariables
         'memberof'
         'msExchPoliciesExcluded'
     )#MultiValuedADAttributesToRetrieve
-    $Script:AllADAttributesToRetrieve = @($ScalarADAttributesToRetrieve + $MultiValuedADAttributesToRetrieve)
-    $Script:AllADContactAttributesToRetrieve = $script:AllADAttributesToRetrieve | Where-Object {$_ -notin ('surName','country','homeMDB','homeMTA','msExchHomeServerName')}
+    $Script:ADUserAttributes = @($script:ScalarADAttributes + $Script:MultiValuedADAttributes)
+    $Script:ADContactAttributes = $script:ADUserAttributes | Where-Object {$_ -notin ('surName','country','homeMDB','homeMTA','msExchHomeServerName')}
+    $Script:ADGroupAttributes = $Script:ADUserAttributes |  Where-Object {$_ -notin ('surName','country','homeMDB','homeMTA','msExchHomeServerName')}
+    $Script:ADGroupAttributesWMembership = $Script:ADGroupAttributes + 'Members' 
     $Script:Stamp = Get-TimeStamp
     #Module Menu Definitions
     $menudefinition = [pscustomobject]@{
@@ -5440,7 +5590,6 @@ function Set-OneShellVariables
         ParentGUID = $null
     }
     Add-MenuDefinition -MenuDefinition $menudefinition
-
     $menudefinition = [pscustomobject]@{
         GUID = '9e7ff8e1-afbb-418d-a31f-9c07bce3ab33'
         Title = 'OneShell Admin User Profile Maintenance'
@@ -5454,7 +5603,6 @@ function Set-OneShellVariables
         ParentGUID = '14aee7c9-6e2a-48bd-bdff-93be72bfc65a'
     }
     Add-MenuDefinition -MenuDefinition $menudefinition
-
     $menudefinition = [pscustomobject]@{
     GUID = 'bfbcf228-1e2e-4289-a7cd-eae003cc3740'
     Title = 'OneShell Organization Profile Maintenance'
