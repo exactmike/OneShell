45094fbad53499bebcdedcb9398080032f3fa525
Mike Campbell <mike@exactsolutions.biz>
2016-07-01 11:31:59 -0400

    Add snips for some work in progress

---
+++ Snips.ps1
@@ -0,0 +1,4 @@
+﻿  $forest = Get-ADForest                                                                                                                                              
+  $schemalocation = "CN=Schema,$($forest.PartitionsContainer.split(',',2)[1])"                                                                                     
+  $attributes = Get-ADObject -SearchBase $schemalocation -filter {ObjectClass -eq 'AttributeSchema'} -Properties *
+  $objecttypes = Get-ADObject -SearchBase $schemalocation -filter {ObjectClass -eq 'classSchema' -and Name -eq 'User'} -Properties *
