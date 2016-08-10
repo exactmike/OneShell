4bb06633d8fe54b1fbdb2f957deeeb9be9cf5ed0
Mike Campbell <mike.campbell@perficient.com>
2016-08-10 17:28:54 -0400

    Fix Export-FunctionsToPSSession

--- OneShell.psm1
+++ OneShell.psm1
@@ -4304,7 +4304,7 @@ $Functions = @(
 #build functions text to initialize in PsSession 
 $FunctionsText = ''
 foreach ($Function in $Functions) {
-    $FunctionText = 'function ' + $Function.Name + "{`r`n" + $Function.Definition + "`r`n}`r`n"
+    $FunctionText = 'function ' + $Function.Name + "`r`n {`r`n" + $Function.Definition + "`r`n}`r`n"
     $FunctionsText = $FunctionsText + $FunctionText
 }
 #convert functions text to scriptblock
@@ -5137,11 +5137,11 @@ if (-not (Test-Path variable:NotesDatabaseConnections))
 }
 if (-not ($NotesSessions.ContainsKey($SessionIdentity)))
 {
-    $NotesSessions.$SessionIdentity = New-Object -ComObject 'Lotus.NotesSession'
-    $NotesSessions.$SessionIdentity.Initialize($Password)
+    $NotesSessions.'$SessionIdentity' = New-Object -ComObject 'Lotus.NotesSession'
+    $NotesSessions.'$SessionIdentity'.Initialize('$Password')
     if (-not ($NotesDatabaseConnections.ContainsKey($Name)))
     {
-        $NotesDatabaseConnections.$Name = $NotesSessions.$SessionIdentity.GetDatabase($ComputerName,$Database)
+        $NotesDatabaseConnections.$Name = $NotesSessions.'$SessionIdentity'.GetDatabase('$ComputerName','$Database')
     }
 }
 Write-Output $NotesDatabaseConnections.$Name
