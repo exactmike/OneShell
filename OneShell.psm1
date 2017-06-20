5524973f2f5c4ba38f1ab1dc7f53dca582e3e656
Mike Campbell <Mike Campbell>
2017-06-20 16:24:32 -0400

    new function new-splitarrayrange

--- OneShell.psm1
+++ OneShell.psm1
@@ -649,6 +649,56 @@ function Start-WindowsSecurity
 }
 function New-GUID {[GUID]::NewGuid()}
 #Conversion and Testing Functions
+function New-SplitArrayRange
+{
+<#  
+  .SYNOPSIS   
+    Provides Start and End Ranges to Split an array into a specified number of parts (new arrays) or parts (new arrays) with a specified number (size) of elements
+  .PARAMETER inArray
+   A one dimensional array you want to split
+  .EXAMPLE  
+   Split-array -inArray @(1,2,3,4,5,6,7,8,9,10) -parts 3
+  .EXAMPLE  
+   Split-array -inArray @(1,2,3,4,5,6,7,8,9,10) -size 3
+  .NOTE
+  Derived from https://gallery.technet.microsoft.com/scriptcenter/Split-an-array-into-parts-4357dcc1#content
+#>
+[cmdletbinding()]
+param(
+  [parameter(Mandatory)]
+  [array]$inputArray
+  ,
+  [parameter(Mandatory,ParameterSetName ='Parts')]
+  [int]$parts
+  ,
+  [parameter(Mandatory,ParameterSetName ='Size')]
+  [int]$size
+)
+  switch ($PSCmdlet.ParameterSetName)
+  {
+    'Parts'
+    {
+      $PartSize = [Math]::Ceiling($inputArray.count / $parts)
+    } 
+    'Size'
+    {
+      $PartSize = $size
+      $parts = [Math]::Ceiling($inputArray.count / $size)
+    }
+  }
+  for ($i=1; $i -le $parts; $i++)
+  {
+    $start = (($i-1)*$PartSize)
+    $end = (($i)*$PartSize) - 1
+    if ($end -ge $inputArray.count) {$end = $inputArray.count}
+    $SplitArrayRange = [pscustomobject]@{
+      Part = $i
+      Start = $start
+      End = $end
+    }
+    Write-Output -InputObject $SplitArrayRange
+  }
+}
 function Convert-HashtableToObject
 {
     [CmdletBinding()]
