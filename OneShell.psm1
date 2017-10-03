a515c12f18ac5a3fa40db4e6028122f424ba1373
exactmike <mike@exactsolutions.biz>
2017-10-03 16:52:55 -0400

    parameter help added for function Get-ArrayIndexForValue

--- OneShell.psm1
+++ OneShell.psm1
@@ -66,13 +66,13 @@ function Get-ArrayIndexForValue
     [cmdletbinding()]
     param(
         [parameter(mandatory=$true)]
-        $array
+        $array #The array for which you want to find a value's index
         ,
         [parameter(mandatory=$true)]
-        $value
+        $value #The Value for which you want to find an index
         ,
         [parameter()]
-        $property
+        $property #The property name for the value for which you want to find an index
     )
     if ([string]::IsNullOrWhiteSpace($Property))
     {
