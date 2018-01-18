9d7d55d6a974749baac1b2e42a388c64ad08836e
exactmike <mike@exactsolutions.biz>
2018-01-18 17:21:11 -0500

    improve Test-EmailAddress function

--- TestFunctions.ps1
+++ TestFunctions.ps1
@@ -22,7 +22,7 @@ function Test-Member
             [Management.Automation.PSMemberViewTypes] 
             $View, 
 
-            [Switch] 
+            [Switch]
             $Static, 
 
             [Switch] 
@@ -222,10 +222,17 @@ Function Test-EmailAddress
         [cmdletbinding()]
         param
         (
-            [string]$EmailAddress
+            [parameter(Mandatory,ValueFromPipeline)]
+            [string[]]$EmailAddress
         )
-        #Regex borrowed from: http://www.regular-expressions.info/email.html
-        $EmailAddress -imatch '^(?=[A-Z0-9][A-Z0-9@._%+-]{5,253}$)[A-Z0-9._%+-]{1,64}@(?:(?=[A-Z0-9-]{1,63}\.)[A-Z0-9]+(?:-[A-Z0-9]+)*\.){1,8}[A-Z]{2,63}$'
+        process
+        {
+            foreach ($ea in $EmailAddress)
+            {
+                #Regex borrowed from: http://www.regular-expressions.info/email.html
+                $ea -imatch '^(?=[A-Z0-9][A-Z0-9@._%+-]{5,253}$)[A-Z0-9._%+-]{1,64}@(?:(?=[A-Z0-9-]{1,63}\.)[A-Z0-9]+(?:-[A-Z0-9]+)*\.){1,8}[A-Z]{2,63}$'
+            }
+        }
     }
 #end function Test-EmailAddress
 function Test-StringIsConvertibleToGUID
