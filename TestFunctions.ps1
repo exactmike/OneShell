3583a3e9d72cd7fb6f26bc5d32bbf7d110191655
Mike Campbell <mike@exactsolutions.biz>
2018-01-25 14:18:32 -0500

    add test function Test-ADPSDrive

--- TestFunctions.ps1
+++ TestFunctions.ps1
@@ -350,3 +350,43 @@ function Test-TCPConnection
         }
     }
 #End function Test-TCPConnection
+function Test-ADPsDrive
+    {
+        [cmdletbinding()]
+        param
+        (
+            [string]$Name
+            ,
+            [switch]$IsRootofDirectory
+        )
+        
+        #Check PSDrive:  Should be AD, Should be Root of the PSDrive
+        Try
+        {
+            $ADPSDrive = Get-PSDrive -name $name -PSProvider ActiveDirectory -ErrorAction Stop
+        }
+        Catch
+        {
+            Write-Verbose -message "No PSDrive with Name $name and PSProviderType ActiveDirectory exists."
+            $false
+        }
+
+        $PSDriveTests = @{
+            ProviderIsActiveDirectory = $($ADPSDrive.Provider.name -eq 'ActiveDirectory')
+        }
+
+        if ($IsRootDSE)
+        {
+            $psdriveTests.RootIsRootOfDirectory = ($ADPSDrive.Root -eq '//RootDSE/')
+        }
+
+        if ($PSDriveTests.Values -contains $false)
+        {
+            $false
+        }
+        else
+        {
+            $true
+        }
+    }
+#end function Test-ADPSDrive
