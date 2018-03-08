982f997d33a0f5d17079fe216feda469b7e972d6
Mike Campbell <mike@exactsolutions.biz>
2018-03-08 12:21:45 -0500

    update New-ExchangeOrganizationDynamicParameter for OneShell v2

--- ExchangeFunctions.ps1
+++ ExchangeFunctions.ps1
@@ -567,3 +567,42 @@ function Get-DuplicateEmailAddresses
         }#Foreach
     }
 }#function
+function New-ExchangeOrganizationDynamicParameter
+{
+    [cmdletbinding()]
+    param
+    (
+        [switch]$Mandatory
+        ,
+        [int]$Position
+        ,
+        [string]$ParameterSetName
+        ,
+        [switch]$Multivalued
+    )
+    $NewDynamicParameterParams=@{
+        Name = 'ExchangeOrganization'
+        ValidateSet = @(
+            Get-OneShellAvailableSystem -ServiceType ExchangeOnPremises,ExchangeOnline,ExchangeComplianceCenter | 
+            ForEach-Object -Process {$_.Name;$_.Identity} | Sort-Object
+        )
+    }
+    if ($PSBoundParameters.ContainsKey('Mandatory'))
+    {
+        $NewDynamicParameterParams.Mandatory = $true
+    }
+    if ($PSBoundParameters.ContainsKey('Multivalued'))
+    {
+        $NewDynamicParameterParams.Type = [string[]]
+    }
+    if ($PSBoundParameters.ContainsKey('Position'))
+    {
+        $NewDynamicParameterParams.Position = $Position
+    }
+    if ($PSBoundParameters.ContainsKey('ParameterSetName'))
+    {
+        $NewDynamicParameterParams.ParameterSetName = $ParameterSetName
+    }
+    New-DynamicParameter @NewDynamicParameterParams
+}
+#end function New-ExchangeOrganizationDynamicParameter
