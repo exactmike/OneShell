ba7f9158f8100b15c83acf4efe5b66c11ca16999
Mike Campbell <mike@exactsolutions.biz>
2018-04-16 12:47:59 -0400

    remove Test-StringIsConvertibleToGUID function and Test-ADPSDrive function

--- TestFunctions.ps1
+++ TestFunctions.ps1
@@ -226,22 +226,11 @@ Function Test-EmailAddress
     }
 }
 #end function Test-EmailAddress
-function Test-StringIsConvertibleToGUID
-{
-    [CmdletBinding()]
-    param
-    (
-        [parameter(Mandatory, ValueFromPipeline)]
-        [String]$string
-    )
-    try {([guid]$string -is [guid])} catch {$false}
-}
-#end function TestStringIsConvertibleToGUID
 function Test-TCPConnection
 {
     <#
             .SYNOPSIS
-            Tests a TCP Connection to each port specified for each ComputerName specified.
+            Tests a TCP Connection to each port specified for each ComputerName specified. Useful on systems that don't have Test-NetConnection.
 
             .DESCRIPTION
             Tests a TCP Connection to each port specified for each ComputerName specified and,
@@ -348,43 +337,3 @@ function Test-TCPConnection
     }
 }
 #End function Test-TCPConnection
-function Test-ADPsDrive
-{
-    [cmdletbinding()]
-    param
-    (
-        [string]$Name
-        ,
-        [switch]$IsRootofDirectory
-    )
-
-    #Check PSDrive:  Should be AD, Should be Root of the PSDrive
-    Try
-    {
-        $ADPSDrive = Get-PSDrive -name $name -PSProvider ActiveDirectory -ErrorAction Stop
-    }
-    Catch
-    {
-        Write-Verbose -message "No PSDrive with Name $name and PSProviderType ActiveDirectory exists."
-        $false
-    }
-
-    $PSDriveTests = @{
-        ProviderIsActiveDirectory = $($ADPSDrive.Provider.name -eq 'ActiveDirectory')
-    }
-
-    if ($IsRootDSE)
-    {
-        $psdriveTests.RootIsRootOfDirectory = ($ADPSDrive.Root -eq '//RootDSE/')
-    }
-
-    if ($PSDriveTests.Values -contains $false)
-    {
-        $false
-    }
-    else
-    {
-        $true
-    }
-}
-#end function Test-ADPSDrive
