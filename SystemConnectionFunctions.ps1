8b6e560f2e016ab1fb1164e4786156217735cd92
exactmike <mike@exactsolutions.biz>
2018-12-06 18:13:56 -0500

    one more mod?

--- SystemConnectionFunctions.ps1
+++ SystemConnectionFunctions.ps1
@@ -394,7 +394,7 @@ function Test-OneShellSystemConnection
                             )
                             if ($null -ne $value)
                             {
-                                $ValueToTest = $ConnectionTestCommandOutput | Select-Object -ExpandProperty $v.Name
+                                $ValueToTest = $ConnectionTestCommandOutput.$($v.Name)
                                 Write-OneShellLog -Message "Testing Expression: '$ValueToTest' $($v.Operator) '$Value'"
                                 Invoke-Expression -Command $("'$ValueToTest' $($v.Operator) '$Value'")
                             }
