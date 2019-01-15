9ef076dad47db2e253785726bcc9cda978288a68
Mike Campbell <mike@exactsolutions.biz>
2019-01-14 23:27:58 -0500

    remove references to monolithic function files from OneShell.psm1

--- OneShell.psm1
+++ OneShell.psm1
@@ -2,14 +2,7 @@
 ##########################################################################################################
 #Import functions from included ps1 files
 ##########################################################################################################
-. $(Join-Path $PSScriptRoot 'UtilityFunctions.ps1')
-. $(Join-Path $PSScriptRoot 'UserInputFunctions.ps1')
-. $(Join-Path $PSScriptRoot 'ProfileFunctions.ps1')
-. $(Join-Path $PSScriptRoot 'TestFunctions.ps1')
-. $(Join-Path $PSScriptRoot 'SkypeOnline.ps1')
-. $(Join-Path $PSScriptRoot 'ParameterFunctions.ps1')
-. $(Join-Path $PSScriptRoot 'LoggingFunctions.ps1')
-. $(Join-Path $PSScriptRoot 'VariableFunctions.ps1')
+
 . $(Join-Path $PSScriptRoot 'RegisterArgumentCompleter.ps1')
 
 $FunctionFiles = Get-ChildItem -Recurse -File -Path $(Join-Path -Path $PSScriptRoot -ChildPath 'Functions')
